b8c5c8f82f3626b3c25b814e416278b944788d6a
Alex Dionicio <alexdionicio934@hotmail.com>
2025-08-24 15:31:46 -0400

    Expand currency mapping: add comprehensive coverage for 200+ countries and territories with proper currency symbols

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -25,6 +25,25 @@ export const COUNTRY_CURRENCY_MAP: { [key: string]: string } = {
   'BZ': 'BZD',  // Belize
   'US': 'USD',  // United States
   'CA': 'CAD',  // Canada
+  'VE': 'VES',  // Venezuela
+  'EC': 'USD',  // Ecuador
+  'GY': 'GYD',  // Guyana
+  'SR': 'SRD',  // Suriname
+  'FK': 'FKP',  // Falkland Islands
+  'GF': 'EUR',  // French Guiana
+  'PR': 'USD',  // Puerto Rico
+  'AW': 'AWG',  // Aruba
+  'CW': 'ANG',  // Curaçao
+  'SX': 'ANG',  // Sint Maarten
+  'BQ': 'USD',  // Caribbean Netherlands
+  'AI': 'XCD',  // Anguilla
+  'VG': 'USD',  // British Virgin Islands
+  'VI': 'USD',  // U.S. Virgin Islands
+  'KY': 'KYD',  // Cayman Islands
+  'TC': 'USD',  // Turks and Caicos
+  'BM': 'BMD',  // Bermuda
+  'GL': 'DKK',  // Greenland
+  'IS': 'ISK',  // Iceland
 
   // Europe
   'ES': 'EUR',  // Spain
@@ -36,6 +55,47 @@ export const COUNTRY_CURRENCY_MAP: { [key: string]: string } = {
   'SE': 'SEK',  // Sweden
   'DK': 'DKK',  // Denmark
   'NO': 'NOK',  // Norway
+  'NL': 'EUR',  // Netherlands
+  'BE': 'EUR',  // Belgium
+  'AT': 'EUR',  // Austria
+  'PT': 'EUR',  // Portugal
+  'IE': 'EUR',  // Ireland
+  'FI': 'EUR',  // Finland
+  'LU': 'EUR',  // Luxembourg
+  'MT': 'EUR',  // Malta
+  'CY': 'EUR',  // Cyprus
+  'EE': 'EUR',  // Estonia
+  'LV': 'EUR',  // Latvia
+  'LT': 'EUR',  // Lithuania
+  'SI': 'EUR',  // Slovenia
+  'SK': 'EUR',  // Slovakia
+  'HR': 'HRK',  // Croatia
+  'BG': 'BGN',  // Bulgaria
+  'RO': 'RON',  // Romania
+  'HU': 'HUF',  // Hungary
+  'CZ': 'CZK',  // Czech Republic
+  'PL': 'PLN',  // Poland
+  'GR': 'EUR',  // Greece
+  'RS': 'RSD',  // Serbia
+  'ME': 'EUR',  // Montenegro
+  'AL': 'ALL',  // Albania
+  'MK': 'MKD',  // North Macedonia
+  'BA': 'BAM',  // Bosnia and Herzegovina
+  'XK': 'EUR',  // Kosovo
+  'MD': 'MDL',  // Moldova
+  'UA': 'UAH',  // Ukraine
+  'BY': 'BYN',  // Belarus
+  'RU': 'RUB',  // Russia
+  'TR': 'TRY',  // Turkey
+  'GE': 'GEL',  // Georgia
+  'AM': 'AMD',  // Armenia
+  'AZ': 'AZN',  // Azerbaijan
+  'KZ': 'KZT',  // Kazakhstan
+  'KG': 'KGS',  // Kyrgyzstan
+  'TJ': 'TJS',  // Tajikistan
+  'TM': 'TMT',  // Turkmenistan
+  'UZ': 'UZS',  // Uzbekistan
+  'MN': 'MNT',  // Mongolia
 
   // Asia
   'JP': 'JPY',  // Japan
@@ -50,13 +110,19 @@ export const COUNTRY_CURRENCY_MAP: { [key: string]: string } = {
   'PH': 'PHP',  // Philippines
   'TW': 'TWD',  // Taiwan
   'HK': 'HKD',  // Hong Kong
-
-  // Oceania
-  'AU': 'AUD',  // Australia
-  'FJ': 'FJD',  // Fiji
+  'BD': 'BDT',  // Bangladesh
+  'PK': 'PKR',  // Pakistan
+  'LK': 'LKR',  // Sri Lanka
+  'NP': 'NPR',  // Nepal
+  'BT': 'BTN',  // Bhutan
+  'MV': 'MVR',  // Maldives
+  'MM': 'MMK',  // Myanmar
+  'LA': 'LAK',  // Laos
+  'KH': 'KHR',  // Cambodia
+  'BN': 'BND',  // Brunei
+  'TL': 'USD',  // East Timor
   'PG': 'PGK',  // Papua New Guinea
-  'SB': 'SBD',  // Solomon Islands
-  'VU': 'VUV',  // Vanuatu
+  'FJ': 'FJD',  // Fiji
   'NC': 'XPF',  // New Caledonia
   'PF': 'XPF',  // French Polynesia
   'WS': 'WST',  // Samoa
@@ -67,6 +133,17 @@ export const COUNTRY_CURRENCY_MAP: { [key: string]: string } = {
   'PW': 'USD',  // Palau
   'MH': 'USD',  // Marshall Islands
   'FM': 'USD',  // Micronesia
+  'GU': 'USD',  // Guam
+  'MP': 'USD',  // Northern Mariana Islands
+  'AS': 'USD',  // American Samoa
+  'CK': 'NZD',  // Cook Islands
+  'NU': 'NZD',  // Niue
+  'TK': 'NZD',  // Tokelau
+  'WF': 'XPF',  // Wallis and Futuna
+  'VU': 'VUV',  // Vanuatu
+  'SB': 'SBD',  // Solomon Islands
+  'AU': 'AUD',  // Australia
+  'NZ': 'NZD',  // New Zealand
 
   // Africa
   'ZA': 'ZAR',  // South Africa
@@ -110,7 +187,37 @@ export const COUNTRY_CURRENCY_MAP: { [key: string]: string } = {
   'BI': 'BIF',  // Burundi
   'RW': 'RWF',  // Rwanda
   'MW': 'MWK',  // Malawi
-  'MZ': 'MZN'   // Mozambique
+  'MZ': 'MZN',  // Mozambique
+  'ST': 'STN',  // São Tomé and Príncipe
+  'CV': 'CVE',  // Cape Verde
+  'GM': 'GMD',  // Gambia
+  'GN': 'GNF',  // Guinea
+  'GW': 'XOF',  // Guinea-Bissau
+  'SL': 'SLE',  // Sierra Leone
+  'LR': 'LRD',  // Liberia
+  'TG': 'XOF',  // Togo
+  'BJ': 'XOF',  // Benin
+  'MR': 'MRU',  // Mauritania
+  'EH': 'MAD',  // Western Sahara
+  'SS': 'SSP',  // South Sudan
+  'GQ': 'XAF',  // Equatorial Guinea
+
+  // Middle East
+  'SA': 'SAR',  // Saudi Arabia
+  'AE': 'AED',  // United Arab Emirates
+  'QA': 'QAR',  // Qatar
+  'KW': 'KWD',  // Kuwait
+  'BH': 'BHD',  // Bahrain
+  'OM': 'OMR',  // Oman
+  'JO': 'JOD',  // Jordan
+  'LB': 'LBP',  // Lebanon
+  'SY': 'SYP',  // Syria
+  'IQ': 'IQD',  // Iraq
+  'IR': 'IRR',  // Iran
+  'AF': 'AFN',  // Afghanistan
+  'PS': 'ILS',  // Palestine
+  'IL': 'ILS',  // Israel
+  'YE': 'YER'   // Yemen
 };
 
 // Currency code to symbol mapping
@@ -197,5 +304,71 @@ export const CURRENCY_SYMBOL_MAP: { [key: string]: string } = {
   'XPF': 'CFP',
   'WST': 'T',
   'TOP': 'T$',
-  'USD': '$'
+  'USD': '$',
+  'VES': 'Bs',
+  'GYD': 'G$',
+  'SRD': '$',
+  'FKP': '£',
+  'AWG': 'ƒ',
+  'ANG': 'ƒ',
+  'XCD': '$',
+  'KYD': '$',
+  'BMD': '$',
+  'ISK': 'kr',
+  'HRK': 'kn',
+  'BGN': 'лв',
+  'RON': 'lei',
+  'HUF': 'Ft',
+  'CZK': 'Kč',
+  'PLN': 'zł',
+  'RSD': 'дин',
+  'ALL': 'L',
+  'MKD': 'ден',
+  'BAM': 'KM',
+  'MDL': 'L',
+  'UAH': '₴',
+  'BYN': 'Br',
+  'RUB': '₽',
+  'TRY': '₺',
+  'GEL': '₾',
+  'AMD': '֏',
+  'AZN': '₼',
+  'KZT': '₸',
+  'KGS': 'с',
+  'TJS': 'ЅМ',
+  'TMT': 'T',
+  'UZS': 'so\'m',
+  'MNT': '₮',
+  'BDT': '৳',
+  'PKR': '₨',
+  'LKR': 'රු',
+  'NPR': 'रू',
+  'BTN': 'Nu',
+  'MVR': 'Rf',
+  'MMK': 'K',
+  'LAK': '₭',
+  'KHR': '៛',
+  'BND': '$',
+  'SAR': 'ر.س',
+  'AED': 'د.إ',
+  'QAR': 'ر.ق',
+  'KWD': 'د.ك',
+  'BHD': 'د.ب',
+  'OMR': 'ر.ع',
+  'JOD': 'د.أ',
+  'LBP': 'ل.ل',
+  'SYP': 'ل.س',
+  'IQD': 'ع.د',
+  'IRR': '﷼',
+  'AFN': '؋',
+  'ILS': '₪',
+  'YER': '﷼',
+  'STN': 'Db',
+  'CVE': '$',
+  'GMD': 'D',
+  'GNF': 'FG',
+  'SLE': 'Le',
+  'LRD': '$',
+  'MRU': 'UM',
+  'SSP': '£'
 };
